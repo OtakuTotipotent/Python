d56acdefa9961ba0993fdf73abb96ea9c76d7080
Afnan Hassan 2.0 <afnanirshad4070@gmail.com>
2024-12-17 10:04:49 +0500

    initial commit

---
+++ Lectures/FileOperations.py
@@ -0,0 +1,16 @@
+# ? Writing & creating a new file
+
+# with open("MyFile.txt", "w") as ourFile:
+#   ourFile.write("Hello, I am a software engineer.\nI am learning python programming")
+
+
+# ourFile = open("MyFile2.docx", "w")
+#   ourFile.write("This is our Document file that can be opened with MS Word.")
+# ourFile.close()
+
+#! ---------------------------------------------------------------
+
+# ? Reading form a file
+
+# with open("myFile.txt", "r") as ourFile:
+#     print(ourFile.read())
--- Lectures/loops.py
+++ Lectures/loops.py
@@ -1,4 +1,5 @@
 num = int(input("Enter number : "))
 
 for i in range(10):
-    print(num, "x", i + 1, "=", num * (i + 1))
+    print(f"{num} x {i+1} = { num*(i+1)}")
+    # print(num, "x", i + 1, "=", num * (i + 1))
